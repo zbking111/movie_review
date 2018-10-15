f058a78c31d1197fb957022ed91bf62ae01e5cf0
tuquyet2709 <goldenlightning1252@gmail.com>
2018-10-15 13:50:56 +0700

    fix layout search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: [:name]
     devise_parameter_sanitizer.permit :account_update, keys: [:name]
+  end
+
+  def set_search
+    @q = Movie.search(params[:q])
   end
 end
