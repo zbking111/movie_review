1c8a8c250a74fe0ef33e78dbbe3baa940fabe419
tuquyet2709 <goldenlightning1252@gmail.com>
2018-10-22 23:10:21 +0700

    fix edit user infomation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get "/add_review", to: "reviews#add"
   get 'reviews/remove'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'static_pages#home'
   get 'static_pages/home'
   match '/search', to: "static_pages#search", via: [:get, :post], as: :search
   resources :movies
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 end
