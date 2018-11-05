ca8bc0328b023a6e51a1479f73733446ca5c40cf
BuiLam <builam1222@gmail.com>
2018-11-05 20:41:54 +0700

    aaaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_155430) do
+ActiveRecord::Schema.define(version: 2018_11_05_034308) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
@@ -62,7 +62,21 @@ ActiveRecord::Schema.define(version: 2018_11_04_155430) do
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'bool' for column 'deactivated'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "role", default: 1
+    t.string "picture"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status"
+    t.integer "detective", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
