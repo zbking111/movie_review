bf41b5914de27cb98e00711382e29a3ce1953706
tuquyet2709 <goldenlightning1252@gmail.com>
2018-11-25 15:44:34 +0700

    fix home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,35 @@
 class StaticPagesController < ApplicationController
   before_action :create_movie, only: [:home, :search]
+  before_action :find_movie, only: [:home, :search]
+
   def home
-    find_movie
-    @movies = Movie.where(check:1).paginate(page: params[:page], per_page: 4).order_desc
+    @top_movies = Movie.where(check:1).rate_score_desc.limit(4)
+    @showing_movies = Movie.where(check: 1).date_7_days_ago.date_desc.limit(4)
+    @show_soon_movies = Movie.where(check: 1).date_30_days_later.date_asc.limit(4)
   end
 
   def search
-    find_movie
+  end
+
+  def top_rating
+    @top_movies = Movie.where(check:1).rate_score_desc
+                      .paginate(page: params[:page], per_page: 8, total_entries: 20)
+  end
+
+  def showing
+    @showing_movies = Movie.where(check: 1).date_7_days_ago.date_desc
+                          .paginate(page: params[:page], per_page: 8)
+  end
+
+  def show_soon
+    @show_soon_movies = Movie.where(check: 1).date_30_days_later.date_asc
+                            .paginate(page: params[:page], per_page: 8)
   end
 
   private
 
   def find_movie
-    @movies = @q.result(distinct: true).includes(:actors).paginate(page: params[:page], per_page: 4)
+    @movies = @q.result(distinct: true).includes(:actors).paginate(page: params[:page], per_page: 8)
   end
 
   def create_movie
