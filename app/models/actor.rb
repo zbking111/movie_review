a74fdb27d8a1c9202b44fb9116326ee110aa914b
bloodarchduke <bloodarchduke@gmail.com>
2018-12-06 09:32:14 +0700

    actor3

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,8 @@
 class Actor < ApplicationRecord
   has_many :movie_characters, foreign_key: "actor_id"
   has_many :movies, through: :movie_characters, source: :movie
+  validates :picture, presence: true
+  mount_uploader :picture, PictureUploader
 
   validates :name, presence: true, length: {maximum: 500}
 end
